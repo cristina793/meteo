3a15990535c0e987423a990fc163d9570ada6bdb
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-01-24 11:03:59 +0100

    changed the searchCity

--- src/index.js
+++ src/index.js
@@ -33,6 +33,9 @@ function formatDate(date){
     let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
     let day= days[date.getDay()];
 
+    if (minutes < 10){
+        minutes=`0${minutes}`};
+
     return`${day} ${hours}:${minutes}`}
 
 function searchCity (city)
@@ -52,4 +55,4 @@ searchCity(searchInput.value);
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Hincesti");
+searchCity("Strasbourg");
